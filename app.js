d10a3b28ab05f733da3f1fc8770399f6e35ac455
asaddeveloper <aiakhokhar@gmail.com>
2020-10-01 22:24:17 +0500

    The updation one

---
+++ app.js
@@ -0,0 +1,31 @@
+//alert("Hello world");
+
+// var a = 5;
+// var b =10;
+// var c=a+b;
+// alert(c);
+
+// var a = 5;
+// var b =10;
+// var c=a+b;
+// console.log (c);
+
+// var x=5;
+// var y="2";
+// var z=x*y;
+// alert(z);
+
+// var a=3;
+// var b=4;
+// var c=7;
+// var d=b-a*c;
+// alert(d);
+
+// var a=5;
+// var b=a++;
+// alert(b);
+// alert(a);
+
+var x=10;
+var y=x++ + ++x -x + x++ - ++x + x;
+alert(y) 
